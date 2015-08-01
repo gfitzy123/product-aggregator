1f26ccdb3163951cb899845b1cf733c15db9ac36
Garrett <gfitzgerald123@gmail.com>
2015-07-31 21:17:58 -0500

    added large collecton

--- product-page-aggregator.js
+++ product-page-aggregator.js
@@ -1,65 +1,34 @@
-
 Products = new Mongo.Collection('products');
-
 
 if (Meteor.isClient) {
     Meteor.startup(function(){
       Session.set('dataLoaded', false);
     });
 
+    var array = Products.find().fetch();
     var productList = [];
     var arrayOfObjects = [];
 
-      Meteor.subscribe('liveProducts', function(){
-      Session.set('dataLoaded', Products.find().fetch());
-      var array = Products.find().fetch();
-   
-
+    Meteor.subscribe('liveProducts', function(){
+        Session.set('dataLoaded', Products.find().fetch());
+        console.log('liveproducts', Session.get('liveProducts'));
+  
         for (var i = 0; i < array.length; i++) {
+          console.log('suup', array[i].product);
           var url = array[i].product[0].images[0].url;
           arrayOfObjects.push(array[i].product[0]);
           arrayOfObjects[i].image = url;
-          console.log('sds', arrayOfObjects)
         }
 
-        Session.set('arrayOfObjects', arrayOfObjects);
-          console.log('arrayofobjects', arrayOfObjects);
-        });
-
-
-    // Grab the collection
-    // Put every property and value in an object
-    // Create helper function, return every value in object in a template
-    // Make a function that is reactive Products
-
-    Template.body.price = function(){
-      return Products.find().fetch()[1].product[0].offerPrice;
-    };
+      Session.set('arrayOfObjects', arrayOfObjects);
+      console.log('arrayofobjects', arrayOfObjects);
+    });
 
     Template.body.helpers({
-      
       products: function(){
         return Session.get('arrayOfObjects');
       }
-      //Session get object
-      //Return it to display on template
-          
     });
-      
-        
-        // console.log(products)
-        // setInterval(function(){
-        //   console.log(products)
-        // }, 1000)
-       
-     
-       
-        // Template.body.helpers({
-        //   returnProducts: function(){
-        //     return JSON.stringify(Products.find().fetch()[0].product);
-        //   }
-        // });
-   
 }
 
 
@@ -67,7 +36,8 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
      Meteor.publish('liveProducts', function() {
       // return Products.remove({});
-      return Products.find({});
+      console.log('publishing', Products.find({}));
+      return Products.find({}, {limit: 100});
     });
   Meteor.startup(function () {
 
@@ -75,8 +45,8 @@ if (Meteor.isServer) {
     // code to run on server at startup
     // HTTP.get('http://api.diffbot.com/v3/product?token=19c08b6b8af2155e6b35859a437eeeb4&url=http://shop.guess.com/en/Catalog/View/women/discounts-deals/low-rise-power-skinny-jeans-with-silicone-rinse/WB1AB2R0YT2&', function(error, result)
 
-      // example product = http://shop.guess.com/en/Catalog/View/women/dresses/natasha-dress/54W7906073Z
-      // example product2 = http://shop.guess.com/en/Catalog/View/women/dresses/sleeveless-pleated-zip-dress/W53K13K3JE0
+    // example product = http://shop.guess.com/en/Catalog/View/women/dresses/natasha-dress/54W7906073Z
+    // example product2 = http://shop.guess.com/en/Catalog/View/women/dresses/sleeveless-pleated-zip-dress/W53K13K3JE0
 
     // HTTP.get('http://api.diffbot.com/v3/analyze?token=19c08b6b8af2155e6b35859a437eeeb4&url=http://shop.guess.com/en/Catalog/View/women/dresses/natasha-dress/54W7906073Z&mode=product&discussion=false', function(error, result) {
     //   console.log('result.content.objects', result.content)
@@ -85,6 +55,11 @@ if (Meteor.isServer) {
     //   Products.insert({product: exampleProduct.objects});
     // });
     
+    // HTTP.get('http://api.diffbot.com/v3/search?token=19c08b6b8af2155e6b35859a437eeeb4&query=type:product', function(error, result) {
+
+
+    //   Products.insert(result.data.objects);
+    // });
 
     // HTTP.get('http://api.diffbot.com/v3/analyze?token=19c08b6b8af2155e6b35859a437eeeb4&url=http://shop.guess.com/en/Catalog/View/women/dresses/sleeveless-pleated-zip-dress/W53K13K3JE0&mode=product&discussion=false', function(error, result) {
     //   console.log('result.content.objects', result.content)
@@ -96,6 +71,3 @@ if (Meteor.isServer) {
   });
 }
 
-// Store crawl in database.
-// Grab image, title, - append to div in page
-// Create new route for clickable image - fetch product page from database
